477baf073394202cc2508a07a751d9756b7fbc26
Antonio Carlos Lima <limaantoniocarlos20@gmail.com>
2021-03-01 01:12:35 -0300

    first commit

---
+++ src/Home/index.tsx
@@ -0,0 +1,176 @@
+import React, { useState } from 'react'
+import { Image, ScrollView, StyleSheet, Text, TextInput, View } from "react-native";
+import { FlatList } from 'react-native-gesture-handler';
+
+const Home = () => {
+  const [value, onChangeText] = React.useState('Useless Placeholder');
+  const [courses, setCourses] = useState([
+    {
+      img: require('../../assets/Math.png'),
+      title: 'Matematica',
+      subtitle: '16 Aulas',
+    },
+    {
+      img: require('../../assets/Math.png'),
+      title: 'Matematica',
+      subtitle: '16 Aulas',
+    },
+    {
+      img: require('../../assets/Math.png'),
+      title: 'Matematica',
+      subtitle: '16 Aulas',
+    },
+    {
+      img: require('../../assets/Math.png'),
+      title: 'Matematica',
+      subtitle: '16 Aulas',
+    },
+    {
+      img: require('../../assets/Math.png'),
+      title: 'Matematica',
+      subtitle: '16 Aulas',
+    },
+    {
+      img: require('../../assets/Math.png'),
+      title: 'Matematica',
+      subtitle: '16 Aulas',
+    },
+    {
+      img: require('../../assets/Math.png'),
+      title: 'Matematica',
+      subtitle: '16 Aulas',
+    },
+    {
+      img: require('../../assets/Math.png'),
+      title: 'Matematica',
+      subtitle: '16 Aulas',
+    }
+  ]);
+
+ 
+  
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <View style={styles.imgHeader}>
+          <Image  source={require('../../assets/Logo.png')} />
+          <Image style={styles.imgSair} source={require('../../assets/Sair.png')} />
+        </View>
+        <View style={styles.containerInput}>
+          <Image source={require('../../assets/Busque.png')}/>
+            <TextInput style={styles.input}
+              onChangeText={text => onChangeText(text)}
+              value={value}
+              placeholder="Busque uma aula"
+          />  
+        </View>
+      </View>
+      <View style={styles.containerCategory}>
+        <View>
+          <Text style={styles.textCategory}>Categorias</Text>
+          <Text style={styles.textNumCursos}>43 cursos</Text>
+        </View>
+        <FlatList 
+          data={courses}
+          numColumns='2'
+          style={{marginHorizontal: 2}}
+          renderItem={({item: course}) => (
+            <View style={styles.containerCourse}>
+              <Image source={course.img}/>
+              <View style={styles.textCategoryItem}>
+                <Text style={styles.textCategoryItemTitle}>{course.title}</Text>
+                <Text>16 Aulas</Text>
+              </View>
+            </View> 
+        )}/>
+      </View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#6548a3',
+  },
+
+  header: {
+    padding: 22,
+    marginVertical: 40,
+  },
+
+  imgHeader: {
+    flexDirection: 'row',
+    position: 'relative',
+
+    
+  },
+
+  imgSair:{
+    position: 'absolute',
+    right: 0
+  },
+
+  containerInput: {
+    marginTop: 20,
+    flexDirection: 'row',
+    alignItems: 'center',
+    borderRadius: 100,
+    padding: 10,
+    
+    backgroundColor: '#fff',
+  },
+
+  input:{
+    margin: 4,
+    color: '#C4C4D1',
+    fontSize: 15
+  },
+
+  containerCategory: {
+    flex: 1,
+    backgroundColor: '#f0edf5',
+    padding: 20
+  },
+
+  textCategory: {
+    fontSize: 20,
+    lineHeight: 20,
+    color: '#3d3d4c'
+  },
+
+  textNumCursos: {
+    position: 'absolute',
+    right: 0
+  },
+
+  containerCourse: {
+    marginTop: 10,
+    backgroundColor: '#fff',
+    width: '46%',
+    justifyContent: 'center',
+    padding: 26,
+    borderRadius: 28,
+    margin: 8
+  },
+
+  textCategoryItemTitle: {
+    fontSize: 16,
+
+  },
+
+  textCategoryItem: {
+    paddingVertical: 10,
+    paddingTop: 12
+  },
+
+  list: {
+    flexDirection: 'row',
+    
+    
+  }
+
+
+})
+
+export default Home;
---
+++ src/Login/index.tsx
@@ -0,0 +1,84 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image, ImageBackground, TouchableOpacity,  Button } from 'react-native';
+import {RectButton} from 'react-native-gesture-handler';
+
+const Login = () => {
+  return(
+    <View  style={styles.container}>
+      <View style={styles.imageBg}>
+      <ImageBackground source={require('../../assets/e.learning.png')} style={styles.image}>
+        <Image source={require('../../assets/Vector.png')}/>
+      </ImageBackground>
+      </View>
+      <View style={styles.containerText}>
+      <Text style={styles.title}>Aprenda da melhor forma</Text>
+        <Text style={styles.subtitle}>Entre na plataforma e
+          acesse cursos de diversas áreas
+         de conhecimento.</Text>
+         <RectButton style={styles.button}>
+         <Text style={styles.buttonText}>Começar os Estudos</Text>
+      </RectButton>
+      </View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#6548a3',
+  },
+
+  image: {
+    flex: 1,
+    resizeMode: 'cover',
+    justifyContent: 'center',
+    padding: 22
+   
+    
+  },
+
+  imageBg: {
+    height: '68%',
+    width: '100%'
+
+    
+  },
+
+  containerText: {
+    marginTop: -70,
+    padding: 22,
+  
+  },
+
+  title: {
+    color: '#ff6680',
+    fontSize: 36,
+
+  },
+
+  subtitle: {
+    color: '#fff',
+    width: '60%',
+    paddingVertical: 24,
+    fontSize: 16,
+    lineHeight: 26
+  },
+
+  button: {
+    backgroundColor: '#ff6680',
+    height: 60,
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'row',
+    borderRadius: 100
+
+  },
+
+  buttonText: {
+    color: '#fff'
+  }
+})
+
+export default Login;
